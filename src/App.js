a9717cc5da62a5a398b1c9dd6e035a80fb1def92
DanielAwaritefe <awaritefe@gmail.com>
2021-06-10 17:24:30 +0100

    added usestate and ternary to dropdown function to close and open

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 
 const items = [
     {
-        title: 'I love React',
-        content: 'React is a frontend framework'
+        title: "I love React",
+        content: "React is a frontend framework"
     },
     {
-        title: 'I love Ruby-on-Rails',
-        content: 'Rails is a server-side web application framework'
+        title: "I love Ruby-on-Rails",
+        content: "Rails is a server-side web application framework"
     },
     {
-        title: 'I love WordPress',
-        content: 'WordPress is an open-source website creation platform'
+        title: "I love WordPress",
+        content: "WordPress is an open-source website creation platform"
     },
 ];
 
 const options = [
   {
-    label: 'See no Evil 🙈',
-    value: 'See'
+    label: "See no Evil",
+    value: "See",
   },
   {
-    label: 'Hear no Evil 🙉',
-    value: 'Hear'
+    label: "Hear no Evil",
+    value: "Hear",
   },
   {
-    label: 'Speak no Evil 🙊',
-    value: 'Speak'
+    label: "Speak no Evil",
+    value: "Speak",
   },
 ];
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      {/* <Accordion items={ items } /> */}
-      <Dropdown options={ options } />
+      <Dropdown 
+        selected={ selected } 
+        onSelectedChange={ setSelected } 
+        options={ options }
+      />
     </div>
   );
 };
+
+export default App;
